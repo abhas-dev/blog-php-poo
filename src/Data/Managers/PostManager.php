845ffed41f4054e03d3000309c58ea56cb11c9fd
Restyled.io <commits@restyled.io>
2021-10-03 22:08:07 +0000

    Restyled by whitespace

--- src/Data/Managers/PostManager.php
+++ src/Data/Managers/PostManager.php
@@ -22,7 +22,7 @@ class PostManager extends Manager
     public function findPostBySlug(int $id): PostModel
     {
         $sql = "
-                SELECT post.*, user.username author 
+                SELECT post.*, user.username author
                 FROM $this->table
                 INNER JOIN user ON post.id_user = user.id
                 WHERE post.id = ?";
